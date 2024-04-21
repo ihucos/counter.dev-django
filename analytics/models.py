aedc9fe1a534c52ee84882fb96e7607cadbe7f6c
Irae Hueck Costa <mail@irae.me>
2024-04-21 09:53:47 +0000

    .

--- analytics/models.py
+++ analytics/models.py
@@ -1,21 +1,31 @@
 from django.db import models
+from django.db.models import F
+from django.db.models.functions import Concat, Cast
 from django.contrib.auth import get_user_model
 
 
 class Domain(models.Model):
+    class Meta:
+        unique_together = [["user", "name"]]
+
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
+
     # publish_key = models.CharField(max_length=64)
+    # published = models.BooleanField()
+
+    def query_counts(self, start_date, end_date):
+        pass
 
     def __str__(self):
         return self.name
 
 
-class DomainStat(models.Model):
+class DomainCount(models.Model):
+    class Meta:
+        unique_together = [["domain", "date", "dimension", "member"]]
 
-    # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
-
     date = models.DateField()
     dimension = models.CharField(max_length=16)
     member = models.CharField(max_length=16)
