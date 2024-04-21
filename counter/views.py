79c2f1828938dcec823cc40c15bfeeeade1312bb
Irae Hueck Costa <mail@irae.me>
2024-04-21 20:09:26 +0000

    .

--- counter/views.py
+++ counter/views.py
@@ -9,7 +9,12 @@ from uuid import UUID
 
 from . import models
 
-ITEM_KEYS = ["user_id", "user_uuid", "domain", "date", "dimension", "member", "count"]
+UPDATE_DOMAIN_COUNT_QUERY = """
+INSERT INTO counter_domaincount (user_id, domain, date, dimension, member, count)
+SELECT * FROM unnest(%s::bigint[], %s::text[], %s::date[], %s::text[], %s::text[], %s::int[])
+ON CONFLICT (user_id, domain, date, dimension, member)
+DO UPDATE SET count = counter_domaincount.count + EXCLUDED.count;
+"""
 
 
 @api_view(["POST"])
@@ -31,7 +36,6 @@ def save_count_batch(request):
 
     user_ids = []
     for username, uuid in zip(request.data["user_id"], request.data["user_uuid"]):
-
         try:
             if username:
                 user_id = user_by_username[username]
@@ -44,12 +48,7 @@ def save_count_batch(request):
 
     with connection.cursor() as cursor:
         cursor.execute(
-            """
-        INSERT INTO counter_domaincount (user_id, domain, date, dimension, member, count)
-        SELECT * FROM UNNEST(%s::bigint[], %s::TEXT[], %s::date[], %s::TEXT[], %s::TEXT[], %s::int[])
-        ON CONFLICT (user_id, domain, date, dimension, member)
-        DO UPDATE SET count = counter_domaincount.count + EXCLUDED.count;
-        """,
+            UPDATE_DOMAIN_COUNT_QUERY,
             [
                 user_ids,
                 request.data["domain"],
