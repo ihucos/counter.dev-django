0deee59b25395acadb214021909d7106cbe87cb1
Irae Hueck Costa <mail@irae.me>
2024-04-21 08:21:11 +0000

    .

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+import asyncio
+from django.http import HttpResponse
+from django.views import View
+
+
+class SaveStatItems(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello async world!")
+
+
+    ```
+       INSERT INTO HotReport (user_identify, domain, date, dimension, member, count)
+       SELECT * FROM UNNEST($1::TEXT[], $2::TEXT[], $3::TEXT[], $4::TEXT[], $5::TEXT[], $6::int[])
+       ON CONFLICT (user_identify, domain, date, dimension, member)
+       DO UPDATE SET count = HotReport.count + 1;`)
+    ```
